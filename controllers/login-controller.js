ef4cdea582c651182ceab55b7b31a56a46733a9f
DanielG <dannyelgomezz.13@gmail.com>
2022-11-23 15:23:29 -0500

    Agregando Funcionalidad basica a login

---
+++ controllers/login-controller.js
@@ -0,0 +1,12 @@
+
+const formulario = document.querySelector('[data-form]');
+formulario.addEventListener('submit', (event) => {
+    event.preventDefault();
+    correo = document.querySelector('[data-email]').value;
+    pass = document.querySelector('[data-pass]').value;
+    if (correo === "1@gmail.com" && pass === "123"){
+        window.location.href = "../screens/lista_productos.html";
+    }else{
+        alert("Correo o Contraseña incorrectas")
+    }
+});

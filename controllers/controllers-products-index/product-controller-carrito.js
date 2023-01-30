7f77786d139d7567081ff8d0a4cfc83ac886b78f
DanielG <dannyelgomezz.13@gmail.com>
2023-01-29 20:33:12 -0500

    Agregando producto seleccionado al Carrito

--- controllers/controllers-products-index/product-controller-carrito.js
+++ controllers/controllers-products-index/product-controller-carrito.js
@@ -1,23 +1,30 @@
 import { productosServices } from '../../services/productos-service.js';
 const container = document.getElementById('product-list');
-container.addEventListener('click', event =>{
-    if (event.target.matches('.get-product')){
+container.addEventListener('click', event => {
+    if (event.target.matches('.get-product')) {
         const productId = event.target.getAttribute('data-product-id');
         console.log(productId);
+
+        const obtenerInformacion = async () => {
+            try {
+                const product = await productosServices.detalleProducto(productId);
+                console.log(product.name);
+                //Seleccionamos el div contendor de productos del carrito
+                const productResults = document.querySelector('.product__list');
+                //Creamos un nuevo elemento dentro del contenedor 
+                const seccion = document.createElement("div");
+                seccion.classList.add("product-item");
+                productResults.appendChild(seccion);
+                const productos = `
+                    <img src="${product.url}" alt="Product 2">
+                    <h3>${product.name}</h3>
+                    <p>${product.price}</p>`
+                    seccion.innerHTML = productos;
+
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        obtenerInformacion();
     }
 });
-
-/* const obtenerInformacion = async () => {
-    const id = "2750b775-0b4e-4d91-80da-790bef6ca87d"
-    try{
-
-        
-        const product = await productosServices.detalleProducto(id);
-        console.log(product);
-        
-    }catch(err){
-        console.log(err);
-    }
-}
-obtenerInformacion();
- */
